217ddf6925d7f121d500e7fe6571a5ce095a81ce
Jackson Sugar <33731112+jacksonsugar@users.noreply.github.com>
2018-10-16 08:28:25 -0400

    Update stream.sh

--- Buoys/Desktop/stream.sh
+++ Buoys/Desktop/stream.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-arecord -D plughw:1,0 -f dat | sshpass -p 'ramboat' ssh -C whale-srv@169.254.21.169 aplay -f dat &
+arecord -D plughw:1,0 -f dat | sshpass -p '*****' ssh -C whale-srv@***** aplay -f dat &
